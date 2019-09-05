cb3e85dff7f5eb9bfb525ae1628b96f3280f16cc
K9xxxx <49764884+K9xxxx@users.noreply.github.com>
2019-09-05 07:55:43 +0200

    Wpisywanie zmieninych danych do pliku

--- Edytuj_kontakt.py
+++ Edytuj_kontakt.py
@@ -1,4 +1,5 @@
 import Dodaj_kontakt
+
 i=0
 arr_im=[]       #Lista imion
 arr_naz=[]      #Lista nazwisk
@@ -19,7 +20,7 @@ with open("Ksiazka Telefoniczna") as f:
 i=int(0)
 
 while i<=ilosc_lini:
-    tab_le.append(len(r.readline())) #Zlicza ilosc znakow z pojedynczej linijki oraz ta wartosc dodaje do listy
+    tab_le.append(len(r.readline())) #Zlicza ilosc znakow z pojedynczej linijki oraz dodaje ta wartosc do listy
     i=i+1
 
 i=int(0)
@@ -35,11 +36,11 @@ while i<=ilosc_lini:
     suma=suma+tab_le[i]
     seek=r.seek(suma+i+1)
     i=i+1
+
+"""Bloki przypisujace odpowiednio numer, imie i nazwisko do odpowiednich list"""
 a=0
 b=1
 c=2
-
-"""Bloki przypisujace odpowiednio numer, imie i nazwisko do odpowiednich list"""
 while a<=len(tab_le):
     try:
         arr_nr_tele.append(tab_new[a])
@@ -61,16 +62,17 @@ while c<=len(tab_le):
 
 i=0
 
+"""Blok kodu odpowiadajacy za wyswietlanie kontaktow w programie"""
 while i+1<=len(arr_nr_tele):
     try:
         print(str(i+1)+". "+arr_im[i]+" "+arr_naz[i]+" ( "+arr_nr_tele[i]+" )")
     except IndexError:
         break
     i=i+1
-print("Ktory kontakt chcesz edytowac?: ", end='')
+print("\nKtory kontakt chcesz edytowac?: ", end='')
 choose=int(input(""))
 
-"""Usuwanie pozycji na listach"""
+"""Usuwanie pozycji w listach"""
 arr_nr_tele.remove(arr_nr_tele[choose-1])
 arr_im.remove(arr_im[choose-1])
 arr_naz.remove(arr_naz[choose-1])
@@ -80,25 +82,31 @@ tab_new.remove(tab_new[choose*3-2])
 tab_new.remove(tab_new[choose*3-3])
 
 print("Wprowadz ponownie kontakt")
-_nr_tele=Dodaj_kontakt.dodaj_nr_tel()
-_imie=Dodaj_kontakt.dodaj_imie()
-_nazwisko=Dodaj_kontakt.dodaj_nazwisko()
+# _nr_tele=Dodaj_kontakt.dodaj_nr_tel()      #Do usuniecia (zrobione w lepszy sposob pod spodem
+# _imie=Dodaj_kontakt.dodaj_imie()           #wywolanie funckcji jako argument
+# _nazwisko=Dodaj_kontakt.dodaj_nazwisko()
 
 """Wprowadza nowa pozycje na miejsce wczesniejszej zmienianej"""
-arr_nr_tele.insert(choose-1,_nr_tele)
-arr_im.insert(choose-1,_imie)
-arr_naz.insert(choose-1,_nazwisko)
+arr_nr_tele.insert(choose-1,Dodaj_kontakt.dodaj_nr_tel())
+arr_im.insert(choose-1,Dodaj_kontakt.dodaj_imie())
+arr_naz.insert(choose-1,Dodaj_kontakt.dodaj_nazwisko())
 
 tab_new.insert(choose*3-3,arr_nr_tele[choose-1])
 tab_new.insert(choose*3-2,arr_im[choose-1])
 tab_new.insert(choose*3-1,arr_naz[choose-1])
 
-"""Blok kodu odpowiadajacy za usuwanie listy by nastepnie ponownie ja wypisac w pliku"""
+"""Blok kodu odpowiadajacy za usuwanie listy ( by nastepnie ponownie ja wypisac w pliku )"""
 with open("Ksiazka Telefoniczna","r") as f:
     lines=f.readlines()
 with open ("Ksiazka Telefoniczna","w") as f:
     for line in lines:
         del line
-"""test"""
+
+"""Wprowadzenie informacji z listy(tab_new) do pliku """
+with open("Ksiazka Telefoniczna", 'w') as f:
+    for number_of_index, index in enumerate(tab_new):
+        if number_of_index!=0:
+            f.write("\n")
+        f.write("{}".format(index))
 
 r.close()
