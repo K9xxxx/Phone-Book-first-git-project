2cf62705bc911af79e0ee48ff319f4303c2e2182
K9xxxx <49764884+K9xxxx@users.noreply.github.com>
2019-09-08 21:14:00 +0200

    Przebudowanie pliku "Edytuj_kontakt.py" w funkcje dodanie nowej mozliwosci w pliku "main" czyli usuniecie kontaktu.

--- Edytuj_kontakt.py
+++ Edytuj_kontakt.py
@@ -1,112 +1,137 @@
 import Dodaj_kontakt
 
-i=0
-arr_im=[]       #Lista imion
-arr_naz=[]      #Lista nazwisk
-arr_nr_tele=[]  #Lista Numerow telefonow
-tab_le=[]       #Lista z iloscia lini
-tab_new=[]      #Lista z kontaktami
-tab_new2=[]     #Lista z kontaktami
-suma=0
+i = 0
+arr_im = []  # Lista imion
+arr_naz = []  # Lista nazwisk
+arr_nr_tele = []  # Lista Numerow telefonow
+tab_le = []  # Lista z iloscia lini
+tab_new = []  # Lista z kontaktami
+tab_new2 = []  # Lista z kontaktami
 
-r=open("Ksiazka Telefoniczna",mode="a+")
-r.seek(0)
+def ilosc_lini():
+    """Przelicza ilosc lini w pliku txt"""
+    with open("Ksiazka Telefoniczna") as f:
+        for i,l in enumerate(f):
+            pass            # Pusta komenda stosowana gdy jest wymagana jakas komenda jak np. tutaj w funkcji
+        ilosc_lini=i
+    return ilosc_lini
 
-"""Przelicza ilosc lini w pliku txt"""
-with open("Ksiazka Telefoniczna") as f:
-    for i,l in enumerate(f):
-        pass            # Pusta komenda stosowana gdy jest wymagana jakas komenda jak np. tutaj w funkcji
-    ilosc_lini=i
-i=int(0)
+def dodanie_do_listy(linie):
+    suma = 0
+    ilosc_lini()
+    r=open("Ksiazka Telefoniczna",mode="a+")
+    r.seek(0)
+    i=int(0)
+    while i<=linie:
+        tab_le.append(len(r.readline())) #Zlicza ilosc znakow z pojedynczej linijki oraz dodaje ta wartosc do listy
+        i=i+1
 
-while i<=ilosc_lini:
-    tab_le.append(len(r.readline())) #Zlicza ilosc znakow z pojedynczej linijki oraz dodaje ta wartosc do listy
-    i=i+1
+    i=int(0)
+    r.seek(0)
 
-i=int(0)
-r.seek(0)
+    """Pentla usuwajaca znak '\n' oraz dodajaca wyrazy z poszczegolnej lini do listy"""
+    while i<=linie:
+        if i==linie:
+            odczyt=r.read(tab_le[i])
+        else:
+            odczyt=r.read(tab_le[i]-1)
+        tab_new.append(odczyt) #dodaje wyraz bez \n do listy
+        suma=suma+tab_le[i]
+        r.seek(suma+i+1)
+        i=i+1
+    r.close()
 
-"""Pentla usuwajaca znak '\n' oraz dodajaca wyrazy z poszczegolnej lini do listy"""
-while i<=ilosc_lini:
-    if i==ilosc_lini:
-        odczyt=r.read(tab_le[i])
-    else:
-        odczyt=r.read(tab_le[i]-1)
-    tab_new.append(odczyt) #dodaje wyraz bez \n do listy
-    suma=suma+tab_le[i]
-    seek=r.seek(suma+i+1)
-    i=i+1
+def sortowanie_do_list():
+    dodanie_do_listy(ilosc_lini())
+    """Bloki przypisujace odpowiednio numer, imie i nazwisko do odpowiednich list"""
+    a = 0
+    b = 1
+    c = 2
+    while a <= len(tab_le):
+        try:
+            arr_nr_tele.append(tab_new[a])
+            a = a + 3
+        except IndexError:
+            break
+    while b <= len(tab_le):
+        try:
+            arr_im.append(tab_new[b])
+            b = b + 3
+        except IndexError:
+            break
+    while c <= len(tab_le):
+        try:
+            arr_naz.append(tab_new[c])
+            c = c + 3
+        except IndexError:
+            break
 
-"""Bloki przypisujace odpowiednio numer, imie i nazwisko do odpowiednich list"""
-a=0
-b=1
-c=2
-while a<=len(tab_le):
-    try:
-        arr_nr_tele.append(tab_new[a])
-        a=a+3
-    except IndexError:
-        break
-while b<=len(tab_le):
-    try:
-        arr_im.append(tab_new[b])
-        b=b+3
-    except IndexError:
-        break
-while c<=len(tab_le):
-    try:
-        arr_naz.append(tab_new[c])
-        c=c+3
-    except IndexError:
-        break
+def pout_listy_kontaktow():
+    """ wyswietla kontakty w programie """
 
-i=0
+    sortowanie_do_list()
 
-"""Blok kodu odpowiadajacy za wyswietlanie kontaktow w programie"""
-while i+1<=len(arr_nr_tele):
-    try:
-        print(str(i+1)+". "+arr_im[i]+" "+arr_naz[i]+" ( "+arr_nr_tele[i]+" )")
-    except IndexError:
-        break
-    i=i+1
-print("\nKtory kontakt chcesz edytowac?: ", end='')
-choose=int(input(""))
+    i=int(0)
+    while i+1<=len(arr_nr_tele):
+        try:
+            print(str(i+1)+". "+arr_im[i]+" "+arr_naz[i]+" ( "+arr_nr_tele[i]+" )")
+        except IndexError:
+            break
+        i=i+1
 
-"""Usuwanie pozycji w listach"""
-arr_nr_tele.remove(arr_nr_tele[choose-1])
-arr_im.remove(arr_im[choose-1])
-arr_naz.remove(arr_naz[choose-1])
+def usuwanie_listy():
+    """Blok kodu odpowiadajacy za usuwanie listy ( by nastepnie ponownie ja wypisac w pliku )"""
+    with open("Ksiazka Telefoniczna","r") as f:
+        lines=f.readlines()
+    with open ("Ksiazka Telefoniczna","w") as f:
+        for line in lines:
+            del line
 
-tab_new.remove(tab_new[choose*3-1])
-tab_new.remove(tab_new[choose*3-2])
-tab_new.remove(tab_new[choose*3-3])
+def usuwanie_pozycji(ktory_kontakt):
+    """Usuwanie pozycji w listach"""
+    arr_nr_tele.remove(arr_nr_tele[ktory_kontakt - 1])
+    arr_im.remove(arr_im[ktory_kontakt - 1])
+    arr_naz.remove(arr_naz[ktory_kontakt - 1])
 
-print("Wprowadz ponownie kontakt")
-# _nr_tele=Dodaj_kontakt.dodaj_nr_tel()      #Do usuniecia (zrobione w lepszy sposob pod spodem
-# _imie=Dodaj_kontakt.dodaj_imie()           #wywolanie funckcji jako argument
-# _nazwisko=Dodaj_kontakt.dodaj_nazwisko()
+    tab_new.remove(tab_new[ktory_kontakt * 3 - 1])
+    tab_new.remove(tab_new[ktory_kontakt * 3 - 2])
+    tab_new.remove(tab_new[ktory_kontakt * 3 - 3])
 
-"""Wprowadza nowa pozycje na miejsce wczesniejszej zmienianej"""
-arr_nr_tele.insert(choose-1,Dodaj_kontakt.dodaj_nr_tel())
-arr_im.insert(choose-1,Dodaj_kontakt.dodaj_imie())
-arr_naz.insert(choose-1,Dodaj_kontakt.dodaj_nazwisko())
+def ponowne_dodanie_z_listy_do_pliku():
+    """Wprowadzenie informacji z listy(tab_new) do pliku """
+    with open("Ksiazka Telefoniczna", 'w') as f:
+        for number_of_index, index in enumerate(tab_new):
+            if number_of_index!=0:
+                f.write("\n")
+            f.write("{}".format(index))
 
-tab_new.insert(choose*3-3,arr_nr_tele[choose-1])
-tab_new.insert(choose*3-2,arr_im[choose-1])
-tab_new.insert(choose*3-1,arr_naz[choose-1])
+def usuwanie_kontaktu():
+    pout_listy_kontaktow()
+    print("\nKtory kontakt chcesz usunac: ",end='')
+    wybor_osoby=int(input())
+    usuwanie_pozycji(wybor_osoby)
+    ponowne_dodanie_z_listy_do_pliku()
 
-"""Blok kodu odpowiadajacy za usuwanie listy ( by nastepnie ponownie ja wypisac w pliku )"""
-with open("Ksiazka Telefoniczna","r") as f:
-    lines=f.readlines()
-with open ("Ksiazka Telefoniczna","w") as f:
-    for line in lines:
-        del line
+def edytuj_kontakt_main():
+    r=open("Ksiazka Telefoniczna", mode="a+")
+    pout_listy_kontaktow()
 
-"""Wprowadzenie informacji z listy(tab_new) do pliku """
-with open("Ksiazka Telefoniczna", 'w') as f:
-    for number_of_index, index in enumerate(tab_new):
-        if number_of_index!=0:
-            f.write("\n")
-        f.write("{}".format(index))
+    print("\nKtory kontakt chcesz edytowac?: ", end='')
+    choose=int(input(""))
 
-r.close()
+    usuwanie_pozycji(choose)
+
+    print("Wprowadz ponownie kontakt")
+    """Wprowadza nowa pozycje na miejsce wczesniejszej ktora zostala poddana zmianie"""
+    arr_nr_tele.insert(choose-1,Dodaj_kontakt.dodaj_nr_tel())
+    arr_im.insert(choose-1,Dodaj_kontakt.dodaj_imie())
+    arr_naz.insert(choose-1,Dodaj_kontakt.dodaj_nazwisko())
+
+    tab_new.insert(choose*3-3,arr_nr_tele[choose-1])
+    tab_new.insert(choose*3-2,arr_im[choose-1])
+    tab_new.insert(choose*3-1,arr_naz[choose-1])
+
+    usuwanie_listy()
+
+    ponowne_dodanie_z_listy_do_pliku()
+    r.close()
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import Dodaj_kontakt
+import Edytuj_kontakt
 
 print("Ksiazka Telefoniczna")
 print("Wybierz numer z mozliwych ponizej:")
-print("1.Dodaj kontakt")
-print("2.Edytuj kontakt")
-print("3.Usun kontakt")
+print("1.Lista kontaktow")
+print("2.Dodaj kontakt")
+print("3.Edytuj kontakt")
+print("4.Usun kontakt")
 x=input("Co wybierasz: ")
 f=open("Ksiazka Telefoniczna","a+")
+
 if x=='1':
+    Edytuj_kontakt.pout_listy_kontaktow()
+elif x=='2':
     nr_tele = Dodaj_kontakt.dodaj_nr_tel()
     f.write("\n"+nr_tele+"\n")
 
@@ -17,7 +22,8 @@ if x=='1':
     nazwisko=Dodaj_kontakt.dodaj_nazwisko()
     f.write(nazwisko)
 
-elif x=='2':
-    pass
-
+elif x=='3':
+    Edytuj_kontakt.edytuj_kontakt_main()
+elif x=='4':
+    Edytuj_kontakt.usuwanie_kontaktu()
 f.close()
