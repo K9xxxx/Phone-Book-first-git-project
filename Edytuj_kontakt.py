81a09094bbc52e9fa45940d8a5848a3acea50cd9
K9xxxx <49764884+K9xxxx@users.noreply.github.com>
2019-09-20 12:20:37 +0200

    added constant values (emergency numbers)

--- Edytuj_kontakt.py
+++ Edytuj_kontakt.py
@@ -1,16 +1,43 @@
 import Dodaj_kontakt
+"""
+Dzialanie programu:
+Program przelicza ilosc lini z pliku txt nastepnie dodaje wartosci tych lini do tablicy ogolnej(tab_new)
+Nastepnie sortuje wartosci z listy tab_new na poszczeglne wyznaczone do odpowiedniej 
+informacji(arr_nr_tele,arr_im,arr_naz)
+Nastepnie wyswietla informacje z tych list dla uzytkownika
+jezeli uzytkownik wprowadzi zmiany to dane ktore zmienial sa zastepowane tymi nowymi na tych samych pozycjach w listach
+Nastpenie usuwana jest cala zawartosc pliku txt oraz ponowne wpisanie wartosci z tablic do pliku
+"""
 
 i = 0
+arr_num_alarm=("997","Policja"," ","998","Straz Pozarna"," ","999","Pogotowie"," ")
 arr_im = []  # Lista imion
 arr_naz = []  # Lista nazwisk
 arr_nr_tele = []  # Lista Numerow telefonow
 tab_le = []  # Lista z iloscia lini
 tab_new = []  # Lista z kontaktami
-tab_new2 = []  # Lista z kontaktami
+
+def dodanie_stalych_do_listy():
+    with open("Ksiazka Telefoniczna",mode='a+') as w:
+        if ilosc_lini()==0 or ilosc_lini()==False:
+            k=0
+            while k <= len(arr_num_alarm):
+                if k != 8:
+                    w.write(arr_num_alarm[k] + '\n')
+                else:
+                    w.write(arr_num_alarm[k])
+                    break
+                k = k + 1
+            print('y')
+        else:
+            print('x')
+            pass
+        w.seek(0)
 
 def ilosc_lini():
+    i=0
     """Przelicza ilosc lini w pliku txt"""
-    with open("Ksiazka Telefoniczna") as f:
+    with open("Ksiazka Telefoniczna",mode='r') as f:
         for i,l in enumerate(f):
             pass            # Pusta komenda stosowana gdy jest wymagana jakas komenda jak np. tutaj w funkcji
         ilosc_lini=i
@@ -18,7 +45,6 @@ def ilosc_lini():
 
 def dodanie_do_listy(linie):
     suma = 0
-    ilosc_lini()
     r=open("Ksiazka Telefoniczna",mode="a+")
     r.seek(0)
     i=int(0)
@@ -39,6 +65,7 @@ def dodanie_do_listy(linie):
         suma=suma+tab_le[i]
         r.seek(suma+i+1)
         i=i+1
+
     r.close()
 
 def sortowanie_do_list():
@@ -65,6 +92,7 @@ def sortowanie_do_list():
             c = c + 3
         except IndexError:
             break
+    print(tab_new)
 
 def pout_listy_kontaktow():
     """ wyswietla kontakty w programie """
@@ -81,14 +109,14 @@ def pout_listy_kontaktow():
 
 def usuwanie_listy():
     """Blok kodu odpowiadajacy za usuwanie listy ( by nastepnie ponownie ja wypisac w pliku )"""
-    with open("Ksiazka Telefoniczna","r") as f:
-        lines=f.readlines()
-    with open ("Ksiazka Telefoniczna","w") as f:
-        for line in lines:
-            del line
+    with open ("Ksiazka Telefoniczna","r+") as f:
+        f.seek(0)
+        for index_of_line,line in enumerate(f):
+            if index_of_line>8:
+                del line
 
 def usuwanie_pozycji(ktory_kontakt):
-    """Usuwanie pozycji w listach"""
+    """Usuwanie okreslonej pozycji w listach"""
     arr_nr_tele.remove(arr_nr_tele[ktory_kontakt - 1])
     arr_im.remove(arr_im[ktory_kontakt - 1])
     arr_naz.remove(arr_naz[ktory_kontakt - 1])
@@ -114,6 +142,7 @@ def usuwanie_kontaktu():
 
 def edytuj_kontakt_main():
     r=open("Ksiazka Telefoniczna", mode="a+")
+    dodanie_stalych_do_listy()
     pout_listy_kontaktow()
 
     print("\nKtory kontakt chcesz edytowac?: ", end='')
